b77750bb200bea91a738020298f433a5f3a23b3e
monali45 <31318232+monali45@users.noreply.github.com>
2019-03-30 19:46:19 +0530

    Add files via upload

---
+++ notify/notify.php
@@ -0,0 +1,320 @@
+
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->isSMTP(true);                                            // Set mailer to use SMTP
+    $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'sihnotifications@gmail.com';                     // SMTP username
+    $mail->Password   = 'travisscott';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('sihnotifications@gmail.com', 'AASHRAY');
+    
+    $mail->addAddress('nayakmonali7@gmail.com', 'provider');     // Add a recipient
+
+    $body = '<p>please deliver the requirements</p>';
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Need for medicine';
+    $mail->Body    = $body;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
+
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>ESIC</title>
+	<meta name="description" content="Ela Admin - HTML5 Admin Template">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
+	<link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
+	<link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+	<link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet">
+
+	<link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet" />
+	<link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" />
+
+	<style>
+		#weatherWidget .currentDesc {
+        color: #ffffff!important;
+    }
+        .traffic-chart {
+            min-height: 335px;
+        }
+        #flotPie1  {
+            height: 150px;
+        }
+        #flotPie1 td {
+            padding:3px;
+        }
+        #flotPie1 table {
+            top: 20px!important;
+            right: -10px!important;
+        }
+        .chart-container {
+            display: table;
+            min-width: 270px ;
+            text-align: left;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+        #flotLine5  {
+             height: 105px;
+        }
+        #flotBarChart {
+            height: 150px;
+        }
+        #cellPaiChart{
+            height: 160px;
+        }
+    </style>
+</head>
+
+<body>
+	<!-- Left Panel -->
+	<aside id="left-panel" class="left-panel">
+		<nav class="navbar navbar-expand-sm navbar-default">
+			<div id="main-menu" class="main-menu collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+                    </li>
+                    <li>
+                        <a href="firstHistory.php"><i class="menu-icon fa fa-laptop"></i>First History</a>
+                    </li>
+                    <li>
+                        <a href="../SampleAvatar/index.php"><i class="menu-icon fa fa-laptop"></i>Treatment analysis</a>
+                    </li>
+                    <li class="active">
+                        <a href="predict.php"><i class="menu-icon fa fa-laptop"></i>Appointment</a>
+                    </li>
+                    <li>
+                        <a href="providers.php"><i class="menu-icon fa fa-laptop"></i>Select Providers</a>
+                    </li>
+                    <li>
+                        <a href="helpline.php"><i class="menu-icon fa fa-laptop"></i>Help Line numbers</a>
+                    </li>
+                    <!--
+                    <li>
+                        <a href="request.php"><i class="menu-icon fa fa-laptop"></i>Request Medicine</a>
+                    </li> -->
+                </ul>
+			</div><!-- /.navbar-collapse -->
+		</nav>
+	</aside>
+	<!-- /#left-panel -->
+	<!-- Right Panel -->
+	<div id="right-panel" class="right-panel">
+		<!-- Header-->
+		<header id="header" class="header">
+			<div class="top-left">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="./"><img src="images/logo.png" alt="Logo"></a>
+					<a class="navbar-brand hidden" href="./"><img src="images/logo2.png" alt="Logo"></a>
+					<a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
+				</div>
+			</div>
+			<div class="top-right">
+				<div class="header-menu">
+					
+
+					<div class="user-area dropdown float-right">
+						<a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<img class="user-avatar rounded-circle" src="images/admin.jpg" alt="User Avatar">
+						</a>
+
+						<div class="user-menu dropdown-menu">
+							<a class="nav-link" href="#"><i class="fa fa- user"></i>My Profile</a>
+
+
+							<a class="nav-link" href="#"><i class="fa fa -cog"></i>Settings</a>
+
+							<a class="nav-link" href="#"><i class="fa fa-power -off"></i>Logout</a>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</header>
+		<!-- /#header -->
+		<!-- Content -->
+		<div class="content">
+			<!-- Animated -->
+			<div class="animated fadeIn">
+
+				<!-- form -->
+				<div class="container">
+					<div class="row">
+                        <h2>Select helpers</h2>
+                        </div><br><br>
+						<div class="row">
+						<form action="" method="post">
+							<div class="row">
+							    <div class="col-lg-6">
+							        
+							</div>
+							
+							
+						</form>
+					
+					</div>
+					<br><br>
+					<div class="row">
+					
+						<h2>Name of the providers</h2>
+						<table class="table table-striped">
+							<thead>
+								<tr>
+									<th>Name</th>
+									<th>Phone no</th>
+									<th>Date of need</th>
+									<th>Requirement</th>
+								
+									
+								</tr>
+							</thead>
+							<tbody>
+							<?php 
+								
+				if(isset($_POST['submit'])){
+				$query = "select * from `provided`";
+				$result = mysqli_query($conn,$query);
+							
+								while($row = mysqli_fetch_array($result)){
+									
+									$fname = $row['fname'];
+									$lname = $row['lname'];
+                                    $phone = $row['phone'];
+									$date = $row['date'];
+									$disc= $row['disc'];
+									echo "<tr>";
+									echo "<td>$fname</td>";
+									echo "<td>$lname</td>";
+									echo "<td>$phone</td>";
+									echo "<td>$date</td>";
+									echo "<td>$disc</td>";
+									
+								}
+								
+								
+								
+								
+							?>
+								
+							</tbody>
+							
+						</table>
+					</div>
+					<br>
+					<div class="row">
+						
+						<table class="table table-striped">
+							<thead>
+								<tr>
+									<th><h2>Doctor's Available</h2></th>
+									
+								</tr>
+							</thead>
+							<tbody>
+							<?php 
+								if(isset($_POST['submit'])){
+							$q1="SELECT `name` from `doctors` where `availability`=1 ";
+							$rwe=mysqli_query($conn,$q1);
+							
+						    
+								while($row2 = mysqli_fetch_array($rwe)){
+									
+									$name = $row2['name'];
+									
+									echo "<tr>";
+									echo "<td>$name</td>";
+									
+								}
+									$_SESSION['max'] = $max;
+								$diff=$count-$max;
+									
+								}
+								
+							
+								
+	
+							?>
+								
+							</tbody>
+							
+						</table>
+					</div>
+					
+				</div>
+				<!-- form end -->
+
+				<!-- /.content -->
+				
+				<!-- Footer -->
+				<footer class="site-footer">
+					<div class="footer-inner bg-white">
+						<div class="row">
+						</div>
+					</div>
+				</footer>
+				<!-- /.site-footer -->
+			</div>
+		</div>
+	</div>
+	
+	
+	
+	
+	
+
+	<!-- Scripts -->
+	<script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
+	<script src="assets/js/main.js"></script>
+
+
+</body>
+
+</html>
